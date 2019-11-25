338eba97fa7efc07a9ac54fee5df37094e86d099
SmartySinghal <sonujain237@gmail.com>
2019-11-25 16:44:32 +0530

    ui changes

---
+++ test_task/js/main.js
@@ -0,0 +1,13 @@
+
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("line_bars").style.display = 'none';
+            document.getElementById("close_icon").style.display = 'block';
+
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("close_icon").style.display = 'none';
+            document.getElementById("line_bars").style.display = 'block';
+        }
